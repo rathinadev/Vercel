1f90c07f0b5a03f5d467598426b38f98dc61dc4e
rathinadev <emrathinadev@gmail.com>
2025-03-12 19:31:53 +0530

    Created endpoint for user to hit to submit URL

--- src/index.ts
+++ src/index.ts
@@ -5,5 +5,13 @@ import cors from "cors";
 const app = express();
 app.use(cors());
 
+
+app.post("/deploy", (req, res) => {
+    const repoUrl = req.body.repoUrl;
+    console.log(repoUrl);
+
+
+    res.json({})
+})
 app.listen(3000);
 console.log("Server started on port 3000");
